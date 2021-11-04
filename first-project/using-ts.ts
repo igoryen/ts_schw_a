a8500fdb7f245f9f9bfc107665e955fa5a07bb48
igoryen <igor.yentaltsev@gmail.com>
2021-11-04 19:20:29 -0400

    exclamation point and type-casting

---
+++ first-project/using-ts.ts
@@ -0,0 +1,16 @@
+const button = document.querySelector("button");
+const input1 = document.getElementById("num1")! as HTMLInputElement; // 1
+const input2 = document.getElementById("num2")! as HTMLInputElement;;
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+button.addEventListener("click", function () {
+  console.log(add(input1.value, input2.value));
+});
+
+/**
+ * 1. the exclamation point means that there will ALWAYS be an element here.
+ *      as HTMLInputElement - is type-casting
+ */
