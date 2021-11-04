293165940eb0922ada40644d1859358df0c5a096
igoryen <igor.yentaltsev@gmail.com>
2021-11-04 19:01:08 -0400

    added a comment

--- first-project/js-only.js
+++ first-project/js-only.js
@@ -8,4 +8,7 @@ function add(num1, num2) {
 
 button.addEventListener("click", function() {
   console.log(add(input1.value, input2.value));
+  // the result is the string "105", 
+  // which is not read "a hundred and five" or "ten oh five"
+  // but "one zero five" because it's a string.
 });
